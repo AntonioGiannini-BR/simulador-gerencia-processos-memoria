7ef2a37a12c7411e0da51af5f3c93182ffcc97e2
AntonioGiannini-BR <antonioeduardo2811@gmail.com>
2026-05-31 16:29:55 -0300

    Add function to print colored memory map

--- mini_so/include/memoria.h
+++ mini_so/include/memoria.h
@@ -17,5 +17,6 @@ int alocar_first_fit(int ram[], PCB* processo);
 void desalocar_memoria(int ram[], int pid);
 void imprimir_mapa_memoria(int ram[]);
 void salvar_log_memoria_final(int ram[], const char* nome_arquivo);
+void imprimirMemoriaColorida();
 
 #endif
